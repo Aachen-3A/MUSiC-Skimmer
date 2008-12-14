c78281f26919ce2c8bbaf2579c30da211079306c
chof <chof@4fa44358-d421-0410-8b48-b5a29ab96256>
2008-12-14 21:20:21 +0000

    enable Lazy-download gives a performance speed-up of 30% while working with dCache:
    without: lazy-download: 15:38 min
    with lazy-download:     11:25 min
    
    running CMSSW_2_2_0 on 1700 QCD Events
    
    
    
    git-svn-id: file:///home/home1/institut_3a/chof/Rep/MIS/trunk/ePaxDemo@600 4fa44358-d421-0410-8b48-b5a29ab96256

--- ePaxAnalyzer/python/RECO_PAT_pxlio.cfg.py
+++ ePaxAnalyzer/python/RECO_PAT_pxlio.cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("singlephoton")
 # initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-#process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
@@ -14,10 +14,18 @@ process.source = cms.Source("PoolSource",
      skipEvents = cms.untracked.uint32(0),
      fileNames = cms.untracked.vstring(
             #'/store/mc/Summer08/SUSY_LM4-sftsht/GEN-SIM-RECO/IDEAL_V9_v1/0000/FC0544C8-C9B1-DD11-B996-001CC47C90A6.root',
-'dcap://grid-dcache.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/dcms/staschmitz/test/single_gamma_2_2_2_relval_1D08E7DEE-B4B9-DD11-B49E-001617E30D00.root'
+#'dcap://grid-dcache.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/dcms/staschmitz/test/single_gamma_2_2_2_relval_1D08E7DEE-B4B9-DD11-B49E-001617E30D00.root'
 #'dcap://grid-dcache.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/dcms/staschmitz/test/Z_ee_2_1_9_relval_04419036-F385-DD11-B3A7-001617C3B6E8_2.root'
+'/store/mc/Summer08/HerwigQCDPt300/GEN-SIM-RECO/IDEAL_V9_v1/0006/04189352-BFA5-DD11-A8A5-00D0680BF8C3.root'
 	)
 )
+
+process.AdaptorConfig = cms.Service("AdaptorConfig",
+    stats = cms.untracked.bool(True),
+    enable = cms.untracked.bool(True),
+    tempDir = cms.untracked.string(""),
+    cacheHint = cms.untracked.string("lazy-download"),
+    readHint = cms.untracked.string("auto-detect"))
 
 process.load("Configuration/StandardSequences/GeometryPilot2_cff")
 
@@ -70,10 +78,16 @@ addJetCollection(process,'kt4CaloJets','KT4',
 addJetCollection(process,'kt6CaloJets','KT6',
                         runCleaner="CaloJet",doJTA=True,doBTagging=True,jetCorrLabel=('KT6','Calo'),doType1MET=True,doL1Counters=False)
 
+process.load("PhysicsTools.HepMCCandAlgos.genEventKTValue_cfi")
 
 import os
 cmsbase = os.environ.get('CMSSW_BASE')
 execfile(cmsbase + "/src/ePaxDemo/ePaxAnalyzer/python/configurePAT_cff")
+
+process.pTHat = cms.EDFilter("PtHatFilter",
+         pt_hat_lower_bound = cms.double(200.),
+         pt_hat_upper_bound = cms.double(500.)
+)
 
 process.ePaxAnalysis = cms.EDAnalyzer("ePaxAnalyzer",
          # label of file:
